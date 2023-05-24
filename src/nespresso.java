d4d1f7a27494ceae6a298948c449509fea46c7d7
Carlos Zerpa <czerpa51@gmail.com>
2023-05-24 17:54:31 -0300

    se actualiza el main

--- src/nespresso.java
+++ src/nespresso.java
@@ -1,3 +1,6 @@
+
+import service.service;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +17,8 @@ public class nespresso {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       service cafe = new service();
+      cafe.servicio();
     }
     
 }
